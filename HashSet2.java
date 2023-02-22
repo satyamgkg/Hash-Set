0a919146a80c76b66530910bf97dfac0b3005d05
satyamgkg <97548391+satyamgkg@users.noreply.github.com>
2023-02-22 19:10:22 +0530

    Add files via upload

---
+++ HashSet2.java
@@ -0,0 +1,25 @@
+// HashSet
+
+
+import java.util.*;  
+
+class HashSet2
+{  
+ public static void main(String args[])
+{  
+   ArrayList<String> list=new ArrayList<String>();  
+
+           list.add("Ravi");  
+           list.add("Vijay");  
+           list.add("Ajay");  
+             
+           HashSet<String> set=new HashSet(list);  
+           set.add("Gaurav");  
+
+           Iterator<String> i=set.iterator();  
+           while(i.hasNext())  
+           {  
+            System.out.println(i.next());  
+           }  
+ }  
+} 
